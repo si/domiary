90c734d7256196ac5dd3cef6aa3740cbd318a8f2
Si Jobling <simon.jobling@gmail.com>
2014-07-24 11:19:10 +0100

    Upgrade WHOIS component for CakePHP 2

--- app/Controller/Component/WhoisComponent.php
+++ app/Controller/Component/WhoisComponent.php
@@ -1,10 +1,9 @@
 <?php
 class WhoisComponent extends Object {
-    function lookup($domain,$deep_whois = true) {
+    public function lookup($domain,$deep_whois = true) {
         App::import('Vendor','whois',array('file'=>'phpwhois'.DS.'whois.main.php'));
         $whois = new Whois();
         $whois->deep_whois = $deep_whois;
         return $whois->Lookup($domain,false);
     }
 }
-?>
