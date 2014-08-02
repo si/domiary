73adc5edea335372c085386fd2a3aed6457bb6b0
mrqwest <mrqwest@gmail.com>
2014-08-02 15:51:15 +0100

    few tweaks, still unable to log in though

--- app/Plugin/Users/View/Elements/Users/sidebar.ctp
+++ app/Plugin/Users/View/Elements/Users/sidebar.ctp
@@ -1,13 +1,22 @@
 <div class="actions">
 	<ul>
+		<!-- if not logged in -->
 		<?php if (!$this->Session->read('Auth.User.id')) : ?>
-			<li><?php echo $this->Html->link(__d('users', 'Login'), array('action' => 'login')); ?></li>
-            <?php if (!empty($allowRegistration) && $allowRegistration)  : ?>
+
+			<!-- login -->
+			<li><?php echo $this->Html->link(__d('users', 'Already a member? log in here'), array('action' => 'login')); ?></li>
+            <?php if (!empty($allowRegistration) && $allowRegistration)  : true ?>
+			<!-- Register -->
 			<li><?php echo $this->Html->link(__d('users', 'Register an account'), array('action' => 'add')); ?></li>
             <?php endif; ?>
 		<?php else : ?>
+			<!-- logout -->
 			<li><?php echo $this->Html->link(__d('users', 'Logout'), array('action' => 'logout')); ?>
+
+			<!-- my account -->
 			<li><?php echo $this->Html->link(__d('users', 'My Account'), array('action' => 'edit')); ?>
+
+			<!-- change password -->
 			<li><?php echo $this->Html->link(__d('users', 'Change password'), array('action' => 'change_password')); ?>
 		<?php endif ?>
 		<?php if($this->Session->read('Auth.User.is_admin')) : ?>
--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -121,33 +121,32 @@ CakeLog::config('error', array(
 
 putenv('TMZ=Europe/London');
 
-	
-  function array_sort_func($a,$b=NULL) { 
-     static $keys; 
-     if($b===NULL) return $keys=$a; 
-     foreach($keys as $k) { 
-        if(@$k[0]=='!') { 
-           $k=substr($k,1); 
-           if(@$a[$k]!==@$b[$k]) { 
-              if(is_numeric(@$a[$k])) {
-                return (@$b[$k] > @$a[$k]); 
-              } else {
-                return strcmp(@$b[$k],@$a[$k]); 
-              }
-           } 
-        } 
-        else if(@$a[$k]!==@$b[$k]) { 
-           return strcmp(@$a[$k],@$b[$k]); 
-        } 
-     } 
-     return 0; 
-  } 
-  
-  function array_sort(&$array) { 
-     if(!$array) return $keys; 
-     $keys=func_get_args(); 
-     array_shift($keys); 
-     array_sort_func($keys); 
-     usort($array,"array_sort_func");        
-  } 
 
+  function array_sort_func($a,$b=NULL) {
+     static $keys;
+     if($b===NULL) return $keys=$a;
+     foreach($keys as $k) {
+        if(@$k[0]=='!') {
+           $k=substr($k,1);
+           if(@$a[$k]!==@$b[$k]) {
+              if(is_numeric(@$a[$k])) {
+                return (@$b[$k] > @$a[$k]);
+              } else {
+                return strcmp(@$b[$k],@$a[$k]);
+              }
+           }
+        }
+        else if(@$a[$k]!==@$b[$k]) {
+           return strcmp(@$a[$k],@$b[$k]);
+        }
+     }
+     return 0;
+  }
+
+  function array_sort(&$array) {
+     if(!$array) return $keys;
+     $keys=func_get_args();
+     array_shift($keys);
+     array_sort_func($keys);
+     usort($array,"array_sort_func");
+  }
